453e71b181834d42c94f4e140492dd3ec972e6a7
Ehtisham <ehtisham2406@gmail.com>
2026-04-04 02:26:36 +0500

    2:26 AM

--- App/Routes/profile.py
+++ App/Routes/profile.py
@@ -1,6 +1,6 @@
 from fastapi import HTTPException,status,Depends,APIRouter
 from sqlalchemy.orm import Session
-from App.Schemas.user_schemas.register_schema import UserResponseSchema
+from App.Schemas.user_schemas.update_schema import UserUpdateSchema
 from App.Security.jwt import get_current_user
 from App.DataModels.user_model import User
 from App.Database.database import get_db
@@ -11,18 +11,24 @@ profile_router=APIRouter()
     status_code=status.HTTP_200_OK,response_model=None
     )
 def get_profile(current_user:User=Depends(get_current_user)):
+    #Only Show the profile of authenticated users
+    return current_user
     
-    if not current_user:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,detail="Please Log in to your Account in order to Access This End point")
-    else:
-        user_profile_details={
-        "Name":current_user.full_name,
-        "Email":current_user.email,
-        "Password":current_user.password,
-        "Phone_number":current_user.phone_number,
-        "Role":current_user.role
-        }
+#--------------------Update user profile----------------------
+@profile_router.put(
+    "/Update_Profile",
+    status_code=status.HTTP_200_OK
+    )
+def Update_Profile(update_data:UserUpdateSchema,current_user:User=Depends(get_current_user),db:Session=Depends(get_db)):
+    if update_data.full_name is not None:
+        current_user.full_name = update_data.full_name
         
-    return user_profile_details
-    
-    
+    if update_data.phone_number is not None:
+        current_user.phone_number = update_data.phone_number
+
+    db.commit()
+
+
+    db.refresh(current_user)
+
+    return {"message": "Profile updated successfully", "user": current_user.full_name}
--- App/Schemas/user_schemas/update_schema.py
+++ App/Schemas/user_schemas/update_schema.py
@@ -1,28 +1,18 @@
-from pydantic import BaseModel, Field
-from App.Schemas.user_schemas.register_schema import PhoneNumber
+from pydantic import BaseModel,Field
+from App.Utils.validator import validate_phone_no
+from typing import Annotated
+from pydantic import AfterValidator
+
+PhoneStr = Annotated[str, AfterValidator(validate_phone_no)]
 # ----------------- Update Request -----------------
-class UserUpdateSchema(PhoneNumber):
+class UserUpdateSchema(BaseModel):
     """
     Schema for capturing Updating credentials.
     No complexity validation is performed here.
     """
     full_name: str = Field(..., min_length=5, max_length=20)
-    
-    class ConfigDict:
-        from_attributes = True
-        str_strip_whitespace = True
-
+    phone_number:PhoneStr
+    model_config={
+        "form_attributes":True
+    }
         
-#----------------------Response-----------------------
-class UserUpdateResponseSchema(BaseModel):
-    """
-    Schema for the data sent back to the user after a successful update.
-    We don't need to inherit validation here, just show the data.
-    """
-    id: int
-    full_name: str
-    phone_number: str
-    email: str  # Usually included so the user sees their current email
-
-    class ConfigDict:
-        from_attributes = True # Allows Pydantic to read SQLAlchemy objects
